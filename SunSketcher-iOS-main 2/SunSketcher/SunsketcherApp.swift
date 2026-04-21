8e6d75be43845f8a2359e49511ca9c8ead025413
Emily Kedenburg <emily.kedenburg@gmail.com>
2026-04-21 08:24:33 -0500

    Changed location elements to August 21st eclipse.

--- SunSketcher-iOS-main 2/SunSketcher/SunsketcherApp.swift
+++ SunSketcher-iOS-main 2/SunSketcher/SunsketcherApp.swift
@@ -26,8 +26,22 @@ struct SunSketcherApp: App {
         }
     }
     
+    // convert `hh:mm:ss` format string to unix time (this version is specifically for Aug. 21, 2017 eclipse)
+        func convertTimes(data: [String]) -> [Int64] {
+            let start = data[0].split(separator: ":").compactMap { Int($0) }
+            let end = data[1].split(separator: ":").compactMap { Int($0) }
+
+            // Add the actual time to the Unix time of UTC midnight for the start of that day
+            // For August 21, 2017
+            let startUnix = 1503273600 + (Int64(start[0]) * 3600) + (Int64(start[1]) * 60) + Int64(start[2])
+            let endUnix = 1503273600 + (Int64(end[0]) * 3600) + (Int64(end[1]) * 60) + Int64(end[2])
+
+            return [startUnix, endUnix]
+        }
+        
+    
     // convert `hh:mm:ss` format string to unix time (this version is specifically for Apr. 8, 2024 eclipse)
-    func convertTimes(data: [String]) -> [Int64] {
+    /*func convertTimes(data: [String]) -> [Int64] {
         let start = data[0].split(separator: ":").compactMap { Int($0) }
         let end = data[1].split(separator: ":").compactMap { Int($0) }
 
@@ -37,7 +51,7 @@ struct SunSketcherApp: App {
         let endUnix = 1712534400 + (Int64(end[0]) * 3600) + (Int64(end[1]) * 60) + Int64(end[2])
 
         return [startUnix, endUnix]
-    }
+    }*/
     
 }
 
