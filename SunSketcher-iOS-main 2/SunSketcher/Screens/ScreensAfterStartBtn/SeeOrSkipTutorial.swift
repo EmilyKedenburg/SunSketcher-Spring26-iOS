bffa334eb683aef90fba3f3ab745f8e6273909cf
Emily Kedenburg <emily.kedenburg@gmail.com>
2026-04-21 09:28:07 -0500

    Fixed iPhone 17 app freeze. This issue was caused by the formatting of SeeOrSkipTutorial. The page has been simplified, but the formatting is not yet fully correct.

--- SunSketcher-iOS-main 2/SunSketcher/Screens/ScreensAfterStartBtn/SeeOrSkipTutorial.swift
+++ SunSketcher-iOS-main 2/SunSketcher/Screens/ScreensAfterStartBtn/SeeOrSkipTutorial.swift
@@ -11,142 +11,73 @@ struct SeeOrSkipTutorial: View {
     @Environment(\.dismiss) private var dismiss
     let prefs = UserDefaults.standard
     
+    let txt = "Press “YES” when asked if it is ok to share photos. To make sure we receive your data, please don’t delete the app until you are notified your data has been sent."
+    
     @Binding var shouldRefreshView: Bool
     
     var body: some View {
-        GeometryReader { geo in
-            NavigationStack{
-                
-                ZStack {
-                    // For background
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: geo.size.width, height: geo.size.height)
-                        .background(Color(red: 0.04, green: 0.04, blue: 0.03))
-                        .offset(x: 0, y: 0)
-                    
-                    // For logo
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.14)
-                        .background(
-                            Image("img-logo")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.07)
-                                .clipped()
-                            
-                        )
-                        .offset(x: 0, y: -geo.size.height * 0.45)
+        NavigationStack {
+            ZStack {
+                Color(red: 0.04, green: 0.04, blue: 0.03)
+                    .ignoresSafeArea()
+
+                VStack {
+                    // Logo
+                    Image("img-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 250)
                         .padding(.top, 20)
-                    
-                    // For eclipse image
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: geo.size.width * 0.85, height: geo.size.height * 1)
-                        .background(
-                            Image("img-eclipse")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: geo.size.width * 1, height: geo.size.height * 1.4)
-                            
-                        )
-                        .offset(x: 0, y: geo.size.height * 0.22)
-                    
-                
-                    VStack {
-                        // This is for the top white line of the background overlay
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: geo.size.width, height: geo.size.height * 0.004)
-                            .background(Color(red: 1, green: 1, blue: 1))
-                            .padding(.top, geo.size.height * 0.11)
-                        
-                        ZStack {
-                            // For dark somewhat clear background overlay
-                            Color.black.opacity(0.4)
-                                .frame(width: geo.size.width * 1, height: geo.size.height * 0.8)
-                                .offset(x: 0, y: geo.size.height * 0.04)
-                            
-                            
-                            VStack {
-                                ScrollView {
-                                    Text("Are you sure you want to skip the tutorial?")
-                                      .font(
-                                        Font.custom("Montserrat", size: geo.size.width * 0.09)
-                                          .weight(.bold)
-                                      )
-                                      .multilineTextAlignment(.center)
-                                      .foregroundColor(.white)
-                                      .frame(width: geo.size.width * 0.8, alignment: .top)
-                                }.frame(width: geo.size.width * 1, height: geo.size.height * 0.2)
-                                
-                                
-                                
-                                
-                                // For buttons
-                                NavigationLink(destination: TutorialScreen1(shouldRefreshView: $shouldRefreshView), label: {
-                                    Text("See tutorial")
-                                        .font(Font.custom("Montserrat", size: geo.size.width * 0.09))
-                                        .fontWeight(.bold)
-                                        .foregroundStyle(.white)
-                                        .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.1)
-                                        .background(Color(red: 0.05, green: 0.58, blue: 0))
-                                        .clipShape(RoundedRectangle(cornerRadius: geo.size.width * 0.04))
-                                        .overlay(RoundedRectangle(cornerRadius: geo.size.width * 0.04)
-                                            .inset(by: -geo.size.width * 0.01)
-                                            .strokeBorder(.white, lineWidth: geo.size.width * 0.008))
-                                })
-                                .frame(width: geo.size.width * 0.62, height: geo.size.height * 0.16)
-                                    .offset(x: 0, y: -geo.size.height * 0)
-                                Button(action: {
-                                    print("Tapped")
-                                    prefs.set(true, forKey: "FromSeeTutorial")
-                                }) {
-                                    
-                                }
-                                
-                                NavigationLink(destination: LocationVerification(), label: {
-                                    Text("Skip tutorial")
-                                        .font(Font.custom("Montserrat", size: geo.size.width * 0.09))
-                                        .fontWeight(.bold)
-                                        .foregroundStyle(.white)
-                                        .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.1)
-                                        .background(Color(red: 0.47, green: 0, blue: 0))
-                                        .clipShape(RoundedRectangle(cornerRadius: geo.size.width * 0.04))
-                                        .overlay(RoundedRectangle(cornerRadius: geo.size.width * 0.04)
-                                            .inset(by: -geo.size.width * 0.01)
-                                            .strokeBorder(.white, lineWidth: geo.size.width * 0.008))
-                                }).frame(width: geo.size.width * 0.62, height: geo.size.height * 0.16)
-                                    .offset(x: 0, y: -geo.size.height * 0)
-                                
-                                
-                                
-                            }// VStack
-                        }// ZStack
-                        .padding(.top, geo.size.width * 0.01)
-                        
-                        // This is for the bottom white line of the background overlay
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: geo.size.width, height: geo.size.height * 0.004)
-                            .background(Color(red: 1, green: 1, blue: 1))
-                            .padding(.top, geo.size.height * 0.028)
-                    }// VStack
-                    
-                }// Zstack
-            }// Navigation stack
-            .navigationBarBackButtonHidden()
-            .onAppear {
-                if prefs.bool(forKey: "FromSeeTutorial") || prefs.bool(forKey: "Close skiptutorial"){
-                    prefs.removeObject(forKey: "FromSeeTutorial")
-                    prefs.removeObject(forKey: "Close skiptutorial")
-                    dismiss()
+
+                    Spacer()
+
+                    // Eclipse image
+                    Image("img-eclipse")
+                        .resizable()
+                        .scaledToFit()
+
+                    Spacer()
+
+                    // Overlay content
+                    VStack(spacing: 20) {
+                        Text("Before you start..")
+                            .font(.custom("Oswald", size: 36))
+                            .bold()
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+
+                        Image("img-dndtutorial")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 200)
+
+                        Text(txt)
+                            .font(.custom("Montserrat", size: 16))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+
+                        NavigationLink(destination: TutorialScreen2(shouldRefreshView: $shouldRefreshView)) {
+                            Image("ic-rightarrow")
+                        }
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.4))
+                    .cornerRadius(12)
+
+                    Spacer()
                 }
             }
-            
-        }// Geometry reader
-    }// body view
+            .navigationBarBackButtonHidden()
+            .onAppear {
+                if prefs.bool(forKey: "Checked") {
+                    prefs.removeObject(forKey: "Checked")
+                    dismiss()
+                }
+            } // ZStack
+        } // NavigationStack
+    } // body view
+
 }
 
 #Preview {
